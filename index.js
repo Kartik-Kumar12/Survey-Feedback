56cbb00f07ccc30b237108fec8b50a5e558914c6
unknown <kartiksaurya05072001@gmail.com>
2020-04-22 14:46:38 +0530

    Changed index.js

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express();
 app.use(bodyParser.urlencoded( {extended : true }));
 app.set('view engine','ejs');
 app.use(express.static('public'));
-app.listen(process.env.port||3000,function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port,function(){
   console.log("Server started successfully at port number 3000");
 });
 
